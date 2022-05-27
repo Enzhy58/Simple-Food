2efad681b8924eec4c05deec8b39c203222a34c0
Enzhy58 <ang.borisova2015@yandex.ru>
2022-05-27 13:22:37 +0300

    added reviews

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,9 @@
 $(function(){
-
+  $('.reviews__slider').slick({
+    dots: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/prev.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/icons/next.svg" alt=""></button>'
+  });
+  var mixer = mixitup('.categories__list');
 });
 
